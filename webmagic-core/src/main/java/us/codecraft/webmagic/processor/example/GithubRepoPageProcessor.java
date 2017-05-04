43cd38a634942073e900312170f4cdbd69917eb5
liuxiongfeng <liuxiongfeng@wondersgroup.com>
2017-05-04 14:15:39 +0800

    对GithubRepoPageProcessor这个例子加注释

--- webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/GithubRepoPageProcessor.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/processor/example/GithubRepoPageProcessor.java
@@ -10,13 +10,16 @@ import us.codecraft.webmagic.processor.PageProcessor;
  * @since 0.3.2
  */
 public class GithubRepoPageProcessor implements PageProcessor {
-
+    // 部分一 抓取网站的相关配置，包括编码、抓取间隔、重试次数等
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setTimeOut(10000);
 
     @Override
+    //process是/***定制***/爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
+        //部分三 从页面发现后续的URL地址来抓取
         page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/[\\w\\-]+/[\\w\\-]+)").all());
         page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/[\\w\\-])").all());
+        //部分二 定义如何抽取页面信息并保存下来
         page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
         page.putField("name", page.getHtml().xpath("//h1[@class='public']/strong/a/text()").toString());
         if (page.getResultItems().get("name")==null){
@@ -32,6 +35,12 @@ public class GithubRepoPageProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(5).run();
+        Spider.create(new GithubRepoPageProcessor())
+                //从"https://github.com/code4craft"开始抓
+                .addUrl("https://github.com/code4craft")
+                //开启5个线程抓取
+                .thread(5)
+                //启动爬虫
+                .run();
     }
 }
